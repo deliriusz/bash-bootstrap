179017f3d4fd9a4741c961d156e125d99f81684e
deliriusz <kalinowskirafal.fm@gmail.com>
2022-08-25 15:16:55 +0200

    updating tmux config

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#programs to install
+git tmux neovim fzf
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
