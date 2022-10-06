ad3304d66566a6b08c5bc93f9bf44bee0bf2eb2a
deliriusz <deliriusz.eth@gmail.com>
2022-10-06 11:36:28 +0200

    improvememtns to init script

--- init.sh
+++ init.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+INSTALL_COMMAND='sudo apt install'
+
 #programs to install
-git tmux neovim fzf node python3 golang
+$INSTALL_COMMAND git tmux neovim python3 golang
 
 # linters for vim to install 
 stylish-haskell stylua devopyio/yamlfmt/yamlfmt brew install lua-language-server
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -16,14 +20,17 @@ curl https://sh.rustup.rs -sSf | sh
 curl https://bootstrap.pypa.io/get-pip.py > get-pip.py; python3 get-pip.py
 pip install yapf pylint python-lsp-server
 go install github.com/mgechev/revive@latest # requires go language
+go install github.com/jesseduffield/lazygit@latest
 cargo install selene
 
 
 # nvim lsp for golang
-dir="${HOME}/.local/share/nvim/site/pack/nvim-lspconfig/opt/nvim-lspconfig/"
+dir="${HOME}/.config/nvim/site/pack/nvim-lspconfig/opt/nvim-lspconfig/"
 mkdir -p "$dir"
 cd "$dir"
 git clone 'https://github.com/neovim/nvim-lspconfig.git' .
 
 npm install -g jsonls html
 
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
