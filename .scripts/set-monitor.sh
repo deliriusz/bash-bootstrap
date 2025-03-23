b2459be3138eef1acfe18f341dc19bc06271b80e
deliriusz <deliriusz.eth@gmail.com>
2025-03-23 11:39:15 +0100

    removing .scripts

--- .scripts/set-monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-CONNECTED_M_COUNT=$(xrandr -q | grep -e '[a-zA-Z]-[0-9] connected' | wc -l)
-
-if (( $CONNECTED_M_COUNT > 1 )); then
-   xrandr --output eDP-1 --off
-else
-   xrandr --output eDP-1 --auto
-fi
